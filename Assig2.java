6c23abb05682399a29e998d764d78f0628cb0558
Courtney C. Dunbar <courtneydunbar336@yahoo.com>
2017-09-08 19:21:08 -0700

    n

---
+++ Assig2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.Math;
+import java.io.File;
+
+public class Assig2 {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
+
+class TripleString{
+	private String string1;
+	private String string2;
+	private String string3;
+	
+	public static final int MAX_LEN = 20;
+	
+	static final int MAX_PULLS=40;
+	static int pullWinnings[] = new int[MAX_PULLS];
+	static int numPulls;
+	
+	TripleString(){
+		
+	}
+	
+}
