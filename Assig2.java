6eca27257bc2239ec03ca32e5e0793b6297f9cf5
CDunba <courtneydunbar336@yahoo.com>
2017-09-11 12:50:31 -0700

    Add files via upload

--- Assig2.java
+++ Assig2.java
@@ -1,15 +1,68 @@
-import java.util.*;
+import java.util.*;   
 import java.lang.Math;
 import java.io.File;
 
-public class Assig2 {
-	
-	
-	public static void main(String[] args) {
-		
-	}
-}
+//notes - get mutators and accessors into Assig2 class, turn tabs into spaces
 
+public class Assig2 {
+	static Scanner user_input = new Scanner( System.in );
+	
+	static int getBet(){
+		int bet;
+		System.out.println("Enter a bet between 0 and 100.");
+		bet = user_input.nextInt();
+		while(bet<0 || bet >100){
+			System.out.println("Enter a bet between 0 and 100.");
+			bet = user_input.nextInt();	
+		}
+		  return bet;
+	}
+	
+	static TripleString pull(){
+		TripleString t = new TripleString();
+		/*
+		 * 
+		 */
+		return t;
+	}
+	
+	static String randString(){
+	  int trigger = (int) (Math.random()*1000);
+	  String rand;
+	  String output[] = new String[4];
+	  
+	  output[0]="BAR";
+	  output[1]="cherries";
+      output[0]="space";
+	  output[0]="7";
+	  
+	  if(trigger<501){
+		  rand=output[0];
+	  } 
+	  if (trigger>500 && trigger<751){
+		  rand=output[1];
+	  }
+	  if(trigger>750 && trigger<875 ){
+		  rand=output[2];
+	  }
+	  else{
+		  rand=output[3];
+	  }
+		
+	  return rand;
+	}
+	
+   public static void main(String[] args) {
+	  TripleString t = new TripleString(); 
+      String a = "a"; 
+	  t.setString1(a); //test 
+	  getBet();
+	 
+	}
+  
+
+
+}
 class TripleString{
 	private String string1;
 	private String string2;
@@ -23,6 +76,61 @@ class TripleString{
 	
 	TripleString(){
 		
+	}	
+	
+	public String toString(){
+		return string1 + " " + string2 + " " + string3;			
+    }
+	
+	boolean saveWinnings(int winnings){
+		for(int i=0;i<=MAX_PULLS;i++){
+			pullWinnings[i]=winnings;
+		}
+		return true;
 	}
 	
+	public String displayWinnings(){
+		String display = "Display: ";
+		for(int i=0;i<=MAX_PULLS;i++){
+			display= display + pullWinnings[i];
+		}
+		return display;
+	}
+	
+	boolean validString( String str ){
+	  if (str instanceof String) {
+		System.out.println("true");
+	    return true;
+	  }
+   	  return false;
+	}
+	
+	public void setString1(String string1)
+    {      
+	  this.string1=string1;
+	  validString(string1);
+	}
+	public void setString2(String string2)
+	{
+	  this.string2=string2;
+      validString(string2);
+    }
+    public void setString3(String string3)
+    {
+      this.string3=string3;
+      validString(string3);
+    }
+    //accessors
+    public String setString1()
+    {
+      return string1;
+    }
+    public String setString2()
+    {
+      return string2;
+    }
+    public String setString3()
+    {
+      return string3;
+    }
 }
